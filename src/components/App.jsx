5621841a8046c934dace5d5ef3e70c2dbe694167
SecaraAdrian <secaforce@gmail.com>
2024-07-01 08:24:31 +0300

    update ex 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,10 @@
 
 import React from "react";
 import Profile from "./profile/Profile"; 
-import user from "../user.json"; 
+import Statistics from "./Statistics";
+import user from "../user.json";
+import data from '../data.json'; 
+
 
 function App() {
   return (
@@ -14,6 +17,7 @@ function App() {
         avatar={user.avatar}
         stats={user.stats} 
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 }
